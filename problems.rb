d9f55e7b2ec581765ebdc2357db506c642b44ff5
Farah Quader <fyquader@gmail.com>
2018-12-12 23:21:16 -0800

    Make function to quicksort

---
+++ problems.rb
@@ -0,0 +1,49 @@
+# QuickSort
+class QuickSort
+  # Quick sort has average case time complexity O(nlogn), but worst
+  # case O(n**2).
+
+  # Not in-place. Uses O(n) memory.
+  def self.sort1(array)
+    return array if array.length === 0
+    pivot = array[0]
+
+    left = array.select { |el| pivot > el }
+    right = array.select { |el| pivot < el }
+    middle = array.select { |el| pivot == el }
+
+    sort1(left) + middle + sort1(right)
+  end
+
+
+  # In-place.
+  def self.sort2!(array, start = 0, length = array.length, &prc)
+    prc ||= Proc.new { |i,j| i <=> j }
+    return array if length <= 1
+
+    pivot_idx = partition(array,start,length,&prc)
+    l_length = pivot_idx - start
+    r_length = length - l_length - 1
+    sort2!(array, start, l_length, &prc)
+    sort2!(array, pivot_idx + 1, r_length, &prc)
+
+    array
+  end
+
+  def self.partition(array, start, length, &prc)
+    prc ||= Proc.new { |i,j| i <=> j }
+    pivot = array[start]
+    pivot_idx = start
+    ((start + 1)...(start + length)).each do |idx|
+      if prc.call(pivot, array[idx]) > 0
+        array[idx], array[pivot_idx + 1] = array[pivot_idx + 1], array[idx]
+        pivot_idx += 1
+      end
+      puts "#{array}--------------"
+    end
+    array[start], array[pivot_idx] = array[pivot_idx], array[start]
+
+    pivot_idx
+  end
+end
+# =======================================================================
